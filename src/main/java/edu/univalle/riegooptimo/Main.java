a4cb32732c3a94a9df338397bf336c5fe009d34b
Edwar-Forero <edwar.forero@correounivalle.edu.co>
2025-10-11 00:10:51 -0500

    Solucion fuerza bruta v1

--- src/main/java/edu/univalle/riegooptimo/Main.java
+++ src/main/java/edu/univalle/riegooptimo/Main.java
@@ -5,8 +5,7 @@ import edu.univalle.riegooptimo.modelo.Finca;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Proyecto Riego Óptimo iniciado");
-        // Ejecución mínima de ejemplo
-        new FuerzaBruta().resolver(new Finca());
+        FuerzaBruta fb = new FuerzaBruta();
+        fb.resolver();
     }
 }
